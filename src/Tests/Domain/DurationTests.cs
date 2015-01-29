d9c5e919496f0442b036509ce5013aab5259e239
Sharon Cichelli <sharon@headspring.com>
2015-01-28 21:15:40 -0600

    Creates custom Fixie convention to support parameterized tests. Refactors Duration tests to use parameterized inputs.

--- src/Tests/Domain/DurationTests.cs
+++ src/Tests/Domain/DurationTests.cs
@@ -5,23 +5,15 @@
 
     public class DurationTests
     {
-        public void Should_split_seconds_into_minutes_and_seconds()
+        [Input(60, 1, 0)]
+        [Input(61, 1, 1)]
+        [Input(45, 0, 45)]
+        [Input(0, 0, 0)]
+        public void Should_split_seconds_into_minutes_and_seconds(int totalSeconds, int expectedMinutes, int expectedSeconds)
         {
-            var duration = new Duration(60);
-            duration.Minutes.ShouldEqual(1);
-            duration.Seconds.ShouldEqual(0);
-
-            var minutesAndSeconds = new Duration(61);
-            minutesAndSeconds.Minutes.ShouldEqual(1);
-            minutesAndSeconds.Seconds.ShouldEqual(1);
-
-            var lessThanAMinute = new Duration(45);
-            lessThanAMinute.Minutes.ShouldEqual(0);
-            lessThanAMinute.Seconds.ShouldEqual(45);
-
-            var zero = new Duration(0);
-            zero.Minutes.ShouldEqual(0);
-            zero.Seconds.ShouldEqual(0);
+            var duration = new Duration(totalSeconds);
+            duration.Minutes.ShouldEqual(expectedMinutes);
+            duration.Seconds.ShouldEqual(expectedSeconds);
         }
     }
 }
