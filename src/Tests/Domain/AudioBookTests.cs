3a58dbe3d9b2d7acb0c2b6b1cc81a9c9cdfb39d5
Sharon Cichelli <sharon@headspring.com>
2015-01-28 21:53:18 -0600

    Adds test for AudioBook's calculation of total duration.

---
+++ src/Tests/Domain/AudioBookTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Tests.Domain
+{
+    using Core.Domain;
+    using Should;
+
+    public class AudioBookTests
+    {
+        public void Should_sum_chapter_durations_to_find_book_duration()
+        {
+            var book = new AudioBook();
+            book.Chapters.Add(new AudioChapter(new Duration(60)));
+            book.Chapters.Add(new AudioChapter(new Duration(55)));
+            book.Chapters.Add(new AudioChapter(new Duration(0)));
+            book.Chapters.Add(new AudioChapter(new Duration(63)));
+            book.GetDuration().Display.ShouldEqual("2:58");
+        }
+    }
+}
