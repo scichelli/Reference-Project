2d5cf2d2ac079f43eab6c66f4e7c559350b5d713
Sharon Cichelli <sharon@headspring.com>
2015-01-28 20:48:29 -0600

    Adds AudioBook classes, minimally implemented.

---
+++ src/Core/Domain/AudioBook.cs
@@ -0,0 +1,30 @@
+﻿namespace Core.Domain
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class AudioBook
+    {
+        public AudioBook()
+        {
+            Chapters = new List<AudioChapter>();
+        }
+
+        public IList<AudioChapter> Chapters { get; private set; }
+
+        public Duration GetDuration()
+        {
+            return new Duration(Chapters.Sum(x => x.Duration.TotalSeconds));
+        }
+    }
+
+    public class AudioChapter
+    {
+        public AudioChapter(Duration duration)
+        {
+            Duration = duration;
+        }
+
+        public Duration Duration { get; private set; }
+    }
+}
---
+++ src/Core/Domain/Duration.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Domain
+{
+    public class Duration
+    {
+        public Duration(int totalSeconds)
+        {
+            TotalSeconds = totalSeconds;
+            //math to split seconds into minutes and seconds
+        }
+
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+        public int TotalSeconds { get; private set; }
+
+        public string Display
+        {
+            get { return string.Format("{0}:{1}", Minutes, Seconds); }
+        }
+
+        public static Duration operator +(Duration a, Duration b)
+        {
+            return new Duration(a.TotalSeconds + b.TotalSeconds);
+        }
+    }
+}
