5075d4a00dab1b0d472783ce69fc2b888fd2b2dc
Sharon Cichelli <sharon@headspring.com>
2015-02-06 17:28:58 -0600

    Adds UI project with MVC, WebAPI, and SPA template components, plus individual-account authentication.

---
+++ src/UI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(UI.Startup))]
+
+namespace UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
