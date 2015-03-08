3d94660e5ea42d7ad939788cb5406016a02f4049
Sharon Cichelli <sharon@headspring.com>
2015-03-08 16:47:52 -0600

    Adds Display Library feature with hard-coded values, so that the next step can introduce Handlers fetched via StructureMap.

---
+++ src/UI/Controllers/LibraryController.cs
@@ -0,0 +1,23 @@
+﻿namespace Headspring.Labs.UI.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Features.Library;
+
+    public class LibraryController : Controller
+    {
+        public ViewResult Index()
+        {
+            var viewModel = new IndexViewModel
+            {
+                Books = new List<IndexViewModel.BookViewModel>
+                {
+                    new IndexViewModel.BookViewModel{Title = "Girl Genius"},
+                    new IndexViewModel.BookViewModel{Title = "American Gods"},
+                    new IndexViewModel.BookViewModel{Title = "Make: Electronics"},
+                }
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ src/UI/Features/Library/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Headspring.Labs.UI.Features.Library
+{
+    using System.Collections.Generic;
+    using System;
+
+    public class IndexViewModel
+    {
+        public IList<BookViewModel> Books { get; set; }
+
+        public class BookViewModel
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+        }
+    }
+}
