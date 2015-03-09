e2afd9c72b1cbe4a30005b7b474bde764c037ef5
Sharon Cichelli <sharon@headspring.com>
2015-03-09 17:46:20 -0600

    Borrows MediatR's Request/Handler pattern. (Will install MediatR later.)

---
+++ src/UI/Features/Library/IndexHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Headspring.Labs.UI.Features.Library
+{
+    using System;
+    using Infrastructure;
+
+    public class IndexHandler : IRequestHandler<IndexQuery, IndexViewModel> 
+    {
+        public IndexViewModel Handle(IndexQuery message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IndexQuery : IRequest<IndexViewModel>
+    {
+    }
+}
