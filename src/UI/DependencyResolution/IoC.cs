cbd744c214ff3296bc877b6d729125c31951e006
Sharon Cichelli <sharon@headspring.com>
2015-03-19 19:13:43 -0500

    Moves curly braces to C#-style positions.

--- src/UI/DependencyResolution/IoC.cs
+++ src/UI/DependencyResolution/IoC.cs
@@ -16,11 +16,14 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 
-namespace Headspring.Labs.UI.DependencyResolution {
+namespace Headspring.Labs.UI.DependencyResolution
+{
     using StructureMap;
-	
-    public static class IoC {
-        public static IContainer Initialize() {
+
+    public static class IoC
+    {
+        public static IContainer Initialize()
+        {
             return new Container(c => c.AddRegistry<DefaultRegistry>());
         }
     }
